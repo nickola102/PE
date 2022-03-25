7935746a4fe87e6400aa3ba8d40aa8c084c8e462
nickola102 <nickola@list.ru>
2022-03-26 00:53:54 +0500

    add test

---
+++ src/test/java/ru/netology/pe/test.java
@@ -0,0 +1,26 @@
+package ru.netology.pe;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class test {
+    @Test
+    void shouldPostSomeData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+}
